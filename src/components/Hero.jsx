2558fbbc876cbb2097a19e8b25b4fba040bfe3e2
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2023-11-23 12:25:42 +0530

    preloaded hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,56 +3,68 @@ import hero from "../assets/hero.webp";
 import interior from "../assets/interior.webp";
 import speedometer from "../assets/bugatti-speedo.webp";
 import bugattiengine from "../assets/bugattiengine.webp";
+import { useEffect } from "react";
 
 const Hero = () => {
+useEffect(() => {
+  preloadImage(hero);
+  preloadImage(interior);
+  preloadImage(speedometer);
+  preloadImage(bugattiengine);
+}, []);
+  const preloadImage = (src) => {
+    const img = new Image();
+    img.src = src;
+  };
+
   return (
     // hero container
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-0  mx-auto mb-16  p-4  max-w-[1100px] sm:mb-20">
       {/* hero left  */}
-      <div className="flex flex-col justify-center  text-center lg:text-left">
-        <h2 className="text-lg font-semibold text-gray-500  ">Drive a Beast</h2>
-        <h2 className="font-extrabold   text-5xl md:text-7xl uppercase  ">
+      <div className="flex flex-col justify-center text-center lg:text-left">
+        <h2 className="text-lg font-semibold text-gray-500 ">Drive a Beast</h2>
+        <h2 className="text-5xl font-extrabold uppercase md:text-7xl ">
           Premium <span className="text-blue-500">cars</span>
         </h2>
         <div>
-          <h4 className="text-xl md:text-3xl font-semibold   mb-2 ">
-            Explore the most <span className="text-yellow-400">luxurious</span>{" "}
+          <h4 className="mb-2 text-xl font-semibold md:text-3xl ">
+            Explore the most <span className="text-yellow-400">luxurious</span>
             cars !
           </h4>
         </div>
-        <div className="flex justify-center  mb-2 lg:justify-start ">
+        <div className="flex justify-center mb-2 lg:justify-start ">
           <div className="flex gap-4">
             <div className="w-20 ">
               <img
                 src={speedometer}
                 alt="speedometer"
-                className="object-cover  rounded-md"
+                className="object-cover rounded-md"
               />
             </div>
             <div className="w-20 ">
               <img
                 src={bugattiengine}
                 alt="bugattiengine"
-                className="object-cover  rounded-md"
+                className="object-cover rounded-md"
               />
             </div>
             <div className="w-20 ">
               <img
                 src={interior}
                 alt="interior"
-                className="object-cover  rounded-md"
+                className="object-cover rounded-md"
               />
             </div>
           </div>
         </div>
         <div>
-          <h4 className="text-lg font-semibold mb-1">Find, Book, Ride</h4>
+          <h4 className="mb-1 text-lg font-semibold">Find, Book, Ride</h4>
         </div>
-        <div className="flex justify-center lg:justify-start font-medium gap-4">
-          <button className="bg-black text-white border-2 border-black  py-2 px-4  text-base rounded hover:bg-yellow-400 transition-all duration-700 ">
+        <div className="flex justify-center gap-4 font-medium lg:justify-start">
+          <button className="px-4 py-2 text-base text-white transition-all duration-700 bg-black border-2 border-black rounded hover:bg-yellow-400 ">
             Take a Ride
           </button>
-          <button className="border-2 border-black  py-2 px-4 text-base rounded hover:bg-black hover:text-white transition-all duration-700 ">
+          <button className="px-4 py-2 text-base transition-all duration-700 border-2 border-black rounded hover:bg-black hover:text-white ">
             Explore
           </button>
         </div>
@@ -62,7 +74,7 @@ const Hero = () => {
         <img
           src={hero}
           alt="audi"
-          className="transform -scale-x-100 object-contain"
+          className="object-contain transform -scale-x-100"
         />
       </div>
     </div>
